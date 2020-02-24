be340703da75ba7f6c97fea494eee6fde37a2e7f
quynh <phamquynh047@gmail.com>
2020-02-24 11:09:37 +0700

    add queue

---
+++ app/Jobs/SendReminderEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Mail\ResetPasswordMail;
+
+class SendReminderEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    protected $data;
+
+    public function __construct()
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        return $this->view('email.sendEmailResetPassword', compact($this->data));
+    }
+}
